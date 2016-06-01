75b4fdf2903f86191d2cd33d6796288a36c3759c
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 13:31:56 +0200

    start adding template refs

--- manifests/kafka/config.pp
+++ manifests/kafka/config.pp
@@ -1,8 +1,11 @@
-class confluent::kafka::config {
+class confluent::kafka::config (
+
+) {
 
   file { '/etc/kafka/server.properties':
-    ensure => file,
-    mode   => '0644',
+    ensure  => file,
+    mode    => '0644',
+    content => template('confluent/kafka/server.properties.erb'),
   }
 
 }
