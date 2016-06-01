4fd50d5fb559dfb3d599e876a0b657e69f91c461
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 09:52:40 +0200

    add debian repo setup

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,16 +1,22 @@
 class confluent::repo::debian {
 
-  file { '/etc/apt/sources.list.d/confluent.list':
-    ensure  => file,
-    mode    => '0644',
-    content => "deb [arch=amd64] http://packages.confluent.io/deb/3.0 stable main\n",
-    notify  => Exec['confluent repo apt update'],
+  apt::key { 'confluent':
+    id      => '670540C841468433',
+    server  => 'pgp.mit.edu',
   }
 
-  exec { 'confluent repo apt update':
-    path        => '/usr/sbin:/usr/bin:/sbin:/bin',
-    command     => 'apt-get update',
-    refreshonly => true,
+  file { '/etc/apt/sources.list.d/confluent.list':
+    ensure  => file,
+    content => 'deb [arch=amd64] http://packages.confluent.io/deb/3.0 stable main',
+    mode    => '0644',
+    notify  => Exec['confluent apt update'],
   }
+
+  exec { 'confluent apt update':
+    path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => 'apt-get -y update',
+    refreshonly => true,  
+  }
+
 
 }
