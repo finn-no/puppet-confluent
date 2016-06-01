9db951601cae49d14a7ceaf32f71cc4b68ac3bac
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 13:40:25 +0200

    one more test, template work

--- manifests/restservice/config.pp
+++ manifests/restservice/config.pp
@@ -6,4 +6,11 @@ class confluent::restservice::config {
     content => template('confluent/restservice/kafka-rest.properties.erb')
   }
 
+  file { '/etc/kafka/kafka-rest-log4j.properties':
+    ensure  => file,
+    mode    => '0644',  
+    content => template('confluent/restservice/kafka-rest-log4j.properties.erb')
+  }
+
+
 }
--- spec/classes/confluent__restservice__config_spec.rb
+++ spec/classes/confluent__restservice__config_spec.rb
@@ -6,6 +6,7 @@ describe 'confluent::restservice::config' do
 
     it { should contain_class('confluent::restservice::config') }
     it { should contain_file('/etc/kafka/kafka-rest.properties') }
+    it { should contain_file('/etc/kafka/kafka-rest-log4j.properties') }
 
   end
 
