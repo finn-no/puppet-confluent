6649da79ffa92441008b55c69a63084fa890af04
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 14:48:45 +0200

    add params to main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,28 @@
 class confluent (  
   $brokerid,
-  $version        = '3.0',
-  $scala_version  = '2.11',
-  $kafka          = false,
-  $zookeeper      = false,
-  $schemaregistry = false,
-  $restservice    = false,
+  $version                              = '3.0',
+  $scala_version                        = '2.11',
+  $kafka                                = false,
+  $zookeeper                            = false,
+  $schemaregistry                       = false,
+  $restservice                          = false,
+  ### subcomnponent defaults, one by one
+  ### kafka
+  $kafkaserver_listen_port              = 7794,
+  $kafkaserver_iothreads                = 8,
+  $kafkaserver_so_rcvbuf                = 1048576,
+  $kafkaserver_so_sndbuf                = 1048576,
+  $kafkaserver_sock_maxbytes            = 104857600,
+  $kafkaserver_replicafetchers          = 2,
+  $kafkaserver_enable_topicdel          = true,
+  $kafkaserver_flush_interval           = 10000,
+  $kafkaserver_flush_scheduler_interval = 2000,
+  $kafkaserver_logretention_minutes     = 4320,
+  $kafkaserver_enable_logcleaner        = true,
+
+  ### zookeeper
+  ### schemaregistry
+  ### restservice
 ) {
 
   if ! ($kafka or $zookeeper or $schemaregistry or $restservice) {
