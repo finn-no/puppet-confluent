f8c2d1e2a7d8391ac73e4ab6db49289dfbab2e67
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 10:11:58 +0200

    add spec tests, ignore fixtures

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 describe 'confluent' do
   context 'with default values for all parameters' do
-    it { should contain_class('confluent') }
+    it do 
+      expect { 
+        is_expected.to raise_error(Puppet::Error, /foo/) 
+      }
+    end
   end
+
+  context 'installing kafka' do
+    let(:params) { { :kafka => true }}
+
+    it { should contain_class('confluent') }
+
+  end
+
 end
