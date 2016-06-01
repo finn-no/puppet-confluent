2eb8ac43070f032944b033ee038223dee0ff4dc8
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-01 14:34:59 +0200

    remove params, no good pattern

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class confluent (
   $zookeeper      = false,
   $schemaregistry = false,
   $restservice    = false,
-) inherits confluent::params {
+) {
 
   if ! ($kafka or $zookeeper or $schemaregistry or $restservice) {
     fail('You must choose to install at least one component')
