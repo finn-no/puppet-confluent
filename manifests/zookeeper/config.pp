19b06ad76c848c0f556531fe4b397990d5d7025f
Kenneth Rørvik <krorvik@gmail.com>
2016-06-15 12:19:58 +0200

    add param to zookeeper configuration

--- manifests/zookeeper/config.pp
+++ manifests/zookeeper/config.pp
@@ -1,5 +1,12 @@
 class confluent::zookeeper::config {
 
+  if $::conflient::zookeeper_myid {
+    file { "$::confluent::zookeeper_datadir/myid":
+      ensure  => file,
+      content => $::conflient::zookeeper_myid,
+      mode    => '0644',      
+  }
+
   ::confluent::propertyfile { '/etc/kafka/zookeeper.properties':
     propertyhash =>$::confluent::zookeeper_properties,
     component    => 'zookeeper',
