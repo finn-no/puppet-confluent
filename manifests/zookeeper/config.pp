d63614f0042be0390aa1e7b1b5ed4472a36f009c
Kenneth Rørvik <krorvik@gmail.com>
2016-06-15 12:39:57 +0200

    add notify to zookeeper config entries

--- manifests/zookeeper/config.pp
+++ manifests/zookeeper/config.pp
@@ -5,12 +5,14 @@ class confluent::zookeeper::config {
       ensure  => file,
       content => $::conflient::zookeeper_myid,
       mode    => '0644',
+      notify       => Service['zookeeper'],
     }
   }
 
   ::confluent::propertyfile { '/etc/kafka/zookeeper.properties':
     propertyhash =>$::confluent::zookeeper_properties,
     component    => 'zookeeper',
+    notify       => Service['zookeeper'],
   }
 
 }
