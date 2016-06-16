9f01478fa756ae75662e561059130106175a04fc
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-16 14:01:31 +0200

    update rspec tests for new repo classes

---
+++ spec/classes/confluent__repo__yum_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'confluent::repo::apt' do
+  context 'with default values for all parameters' do
+
+    let(:facts) { { 
+      :osfamily => 'Debian',
+    }}
+
+    it { should contain_class('confluent::repo::apt') }
+    it { should contain_apt__key('confluent') }
+    it { should contain_exec('repo update') }
+    it { should contain_file('/etc/apt/sources.list.d/confluent.list') }
+
+  end
+
+end
--- spec/classes/confluent__repo_spec.rb
+++ spec/classes/confluent__repo_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe 'confluent::repo' do
+
   context 'with default values for all parameters' do
 
-    let(:facts) { { 
-      :osfamily => 'Debian',
+    let (:facts) {{
+      'osfamily' => 'Debian',
     }}
 
     it { should contain_class('confluent::repo') }
-    it { should contain_apt__key('confluent') }
-    it { should contain_exec('repo update') }
-    it { should contain_file('/etc/apt/sources.list.d/confluent.list') }
 
   end
 
