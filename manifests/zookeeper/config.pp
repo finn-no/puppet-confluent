5b3efa2e3ea02a6ee116a3ad7cc911c0adf3007b
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-06-27 12:22:38 +0200

    deps for zookeeper config, make sure varlib exists

--- manifests/zookeeper/config.pp
+++ manifests/zookeeper/config.pp
@@ -1,11 +1,17 @@
 class confluent::zookeeper::config {
 
   if $::confluent::zookeeper_myid {
-    file { "$::confluent::zookeeper_datadir/myid":
+
+    file { "$::confluent::zookeeper_datadir":
+      ensure => directory,
+    }      
+
+    file { "${::confluent::zookeeper_datadir}/myid":
       ensure  => file,
       content => $::confluent::zookeeper_myid,
       mode    => '0644',
-      notify       => Service['zookeeper'],
+      notify  => Service['zookeeper'],
+      require => Package["confluent-kafka-${::confluent::scala_version}"],
     }
   }
 
@@ -13,6 +19,7 @@ class confluent::zookeeper::config {
     propertyhash =>$::confluent::zookeeper_properties,
     component    => 'zookeeper',
     notify       => Service['zookeeper'],
+    require => Package["confluent-kafka-${::confluent::scala_version}"],
   }
 
 }
